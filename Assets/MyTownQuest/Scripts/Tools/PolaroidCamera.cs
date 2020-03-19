ee3cfcbed81ba222a85b611d097df5f4950efeb8
johnjoemcbob <mcormack1994@hotmail.co.uk>
2020-03-19 11:39:18 +0000

    Camera printing photo, new release

--- Assets/MyTownQuest/Scripts/Tools/PolaroidCamera.cs
+++ Assets/MyTownQuest/Scripts/Tools/PolaroidCamera.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 using VRTK;
 
 public class PolaroidCamera : MonoBehaviour
@@ -8,14 +9,19 @@ public class PolaroidCamera : MonoBehaviour
 	public float LerpSpeed = 5;
 	public float ButtonLerpSpeed = 5;
 	public float BodyHitAngle = 30;
+	public float PolaroidMove = 1;
+	public int PrintSegments = 5;
 
 	[Header( "References" )]
 	public Transform Button;
+	public Transform PolaroidPrintParent;
 
 	[Header( "Assets" )]
 	public RenderTexture Texture;
+	public GameObject PolaroidPrefab;
 
-    private ImgurClient imgurClient;
+	private ImgurClient imgurClient;
+	private GameObject CurrentPolaroid;
 
 	private void OnEnable()
 	{
@@ -50,6 +56,8 @@ public class PolaroidCamera : MonoBehaviour
 
 	public void TakePhoto()
 	{
+		if ( CurrentPolaroid != null ) return;
+
 		// Logic
 		Texture2D tex = new Texture2D( Texture.width, Texture.height );
 		{
@@ -59,6 +67,9 @@ public class PolaroidCamera : MonoBehaviour
 		}
 		string base64 = System.Convert.ToBase64String( tex.EncodeToPNG() );
 		imgurClient.UploadImage( base64 );
+
+		// Print
+		PrintPhoto( tex );
 
 		// Audio effects
 		MyTownQuest.SpawnResourceAudioSource( "camera", transform.position, 1, 5 );
@@ -70,6 +81,23 @@ public class PolaroidCamera : MonoBehaviour
 
 		// Light flash
 		StartCoroutine( Flash() );
+	}
+
+	private void PrintPhoto( Texture2D tex )
+	{
+		// Create print polaroid with texture assigned at print pos
+		GameObject polaroid = Instantiate( PolaroidPrefab, PolaroidPrintParent );
+		{
+			polaroid.transform.localPosition = Vector3.zero;
+			polaroid.transform.localEulerAngles = Vector3.zero;
+			polaroid.transform.localScale = Vector3.one;
+
+			polaroid.GetComponentInChildren<RawImage>().texture = tex;
+		}
+		CurrentPolaroid = polaroid;
+
+		// Start coroutine to print out slowly and then unparent
+		StartCoroutine( PrintProcess() );
 	}
 
 	public void OnUseWhileHeld( object sender, InteractableObjectEventArgs e )
@@ -86,5 +114,30 @@ public class PolaroidCamera : MonoBehaviour
 		yield return new WaitForSeconds( dur );
 
 		FindObjectOfType<VRTK_HeadsetFade>().Unfade( dur );
+	}
+
+	public IEnumerator PrintProcess()
+	{
+		for ( int seg = 0; seg < PrintSegments; seg++ )
+		{
+			// Audio first
+			MyTownQuest.SpawnResourceAudioSource( "print", transform.position, 1, 0.5f );
+			// Visual bump
+			transform.GetChild( 0 ).localScale = new Vector3( 1.1f, 1.2f, 1.1f );
+
+			// Once audio is played then its time to print
+			yield return new WaitForSeconds( 0.5f );
+
+			// Move polaroid downward
+			CurrentPolaroid.transform.localPosition += new Vector3( 0, -1, 0 ) * PolaroidMove / PrintSegments;
+
+			yield return new WaitForEndOfFrame();
+		}
+
+		// Finally unparent the polaroid
+		CurrentPolaroid.transform.SetParent( null );
+		CurrentPolaroid = null;
+
+		yield break;
 	}
 }
